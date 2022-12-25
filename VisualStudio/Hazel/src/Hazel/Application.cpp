25aa7f0742eab4b5e7e4fe81cbe8a158d2279fa0
AndrejLapin <lapin.andrej1@gmail.com>
2022-12-26 01:45:24 +0200

    Day 4 progress, Events

--- VisualStudio/Hazel/src/Hazel/Application.cpp
+++ VisualStudio/Hazel/src/Hazel/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Log.h"
+
 namespace Hazel {
 
     Application::Application()
@@ -12,9 +15,12 @@ namespace Hazel {
 
     void Application::Run()
     {
-        while (true)
+        WindowResizeEvent e(1280, 720);
+        if (e.IsInCategory(EventCategoryInput))
         {
-
+            HZ_TRACE(e);
         }
+
+        while (true);
     }
 }
