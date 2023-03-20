959698391a4fe80f172b6c3de0339a5f5bb2fb78
AndrejLapin <lapin.andrej1@gmail.com>
2023-03-20 20:28:31 +0200

    rendering context

--- Hazel/Hazel/src/Hazel/Core.h
+++ Hazel/Hazel/src/Hazel/Core.h
@@ -15,8 +15,8 @@
 #endif
 
 #ifdef HZ_ENABLE_ASSERTS
-    #define HZ_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
-    #define HZ_CORE_ASSERT(x, ...) { if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+    #define HZ_ASSERT(x, ...) do{ if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }while(0)
+    #define HZ_CORE_ASSERT(x, ...) do{ if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }while(0)
 #else
     #define HZ_ASSERT(x, ...)
     #define HZ_CORE_ASSERT(x, ...)
