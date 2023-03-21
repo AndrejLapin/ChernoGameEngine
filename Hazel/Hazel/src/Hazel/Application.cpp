10a16c2b2cd116fdfc8c1f244daafaea8a142264
AndrejLapin <lapin.andrej1@gmail.com>
2023-03-21 22:26:34 +0200

    first triangle

--- Hazel/Hazel/src/Hazel/Application.cpp
+++ Hazel/Hazel/src/Hazel/Application.cpp
@@ -23,6 +23,29 @@ namespace Hazel {
 
         m_ImGuiLayer = new ImGuiLayer();
         PushOverlay(m_ImGuiLayer);
+
+        glGenVertexArrays(1, &m_VertexArray);
+        glBindVertexArray(m_VertexArray);
+
+        glGenBuffers(1, &m_VertexBuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffer);
+
+        float verticies[3 * 3] = {
+            -0.5f, -0.5f,  0.0f,
+             0.5f, -0.5f,  0.0f,
+             0.0f,  0.5f,  0.0f,
+        };
+
+        glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+
+        glGenBuffers(1, &m_IndexBuffer);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBuffer);
+
+        unsigned int indecies[3] = { 0, 1, 2 };
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indecies), indecies, GL_STATIC_DRAW);
     }
 
     Application::~Application()
@@ -46,7 +69,7 @@ namespace Hazel {
         EventDispatcher dispatcher(e);
         dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(Application::OnWindowCLosed));
 
-        HZ_CORE_INFO("{0}", e);
+        //HZ_CORE_INFO("{0}", e);
 
         for (auto it = m_LayerStack.end(); it != m_LayerStack.begin();)
         {
@@ -60,6 +83,12 @@ namespace Hazel {
     {
         while (m_Running)
         {
+            glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glBindVertexArray(m_VertexArray);
+            glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
+
             for (Layer* layer : m_LayerStack)
                 layer->OnUpdate();
 
@@ -69,7 +98,7 @@ namespace Hazel {
             m_ImGuiLayer->End();
 
             m_Window->OnUpdate();
-            glClear(GL_COLOR_BUFFER_BIT);
+            //glClear(GL_COLOR_BUFFER_BIT);
         }
     }
 
