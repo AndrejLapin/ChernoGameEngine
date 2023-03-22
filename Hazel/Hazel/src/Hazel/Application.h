853cbee34bd010864dc8bb61d9c018803a23440d
AndrejLapin <lapin.andrej1@gmail.com>
2023-03-22 22:07:34 +0200

    shaders!

--- Hazel/Hazel/src/Hazel/Application.h
+++ Hazel/Hazel/src/Hazel/Application.h
@@ -6,6 +6,8 @@
 #include "Hazel/Events/Event.h"
 #include "Hazel/Events/ApplicationEvent.h"
 
+#include "Hazel/Renderer/Shader.h"
+
 namespace Hazel {
 
     class WindowCloseEvent;
@@ -36,6 +38,7 @@ namespace Hazel {
         LayerStack m_LayerStack;
 
         unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+        std::unique_ptr<Shader> m_Shader;
     private:
         static Application* s_Instance;
     };
